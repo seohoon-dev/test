63f0373701cfc025b464e7014c4fcf7847faadd4
seohoon-dev <sh931123@gmail.com>
2019-11-13 18:38:38 +0900

    회원가입 기능 구현

---
+++ src/main/java/com/example/test/web/UserController.java
@@ -0,0 +1,13 @@
+package com.example.test.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	@PostMapping("/create")
+	public String create(User user) {
+		System.out.println("user: " + user);
+		return "index";
+	}
+}
