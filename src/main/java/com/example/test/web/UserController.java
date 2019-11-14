fb3393f8f965433e8a1b2d996cfd840c793e1b6c
seohoon-dev <sh931123@gmail.com>
2019-11-14 16:21:40 +0900

    h2, jpa 사용

--- src/main/java/com/example/test/web/UserController.java
+++ src/main/java/com/example/test/web/UserController.java
@@ -3,26 +3,32 @@ package com.example.test.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.test.domain.User;
+import com.example.test.domain.UserRepository;
+
 @Controller
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/user/create")
 	public String create(User user) {
 		System.out.println("user: " + user);
-		users.add(user);
+		userRepository.save(user);
 		
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 }
